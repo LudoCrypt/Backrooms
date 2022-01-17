a622f6a5586c4e0384028d22f17f883e0d5faebd
LudoCrypt <60561627+LudoCrypt@users.noreply.github.com>
2022-01-16 18:04:29 -0600

    everything gone is just better lol

--- src/main/java/net/ludocrypt/backrooms/access/SanityManagerAccess.java
+++
@@ -1,7 +0,0 @@
-package net.ludocrypt.backrooms.access;
-
-import net.ludocrypt.backrooms.sanity.SanityManager;
-
-public interface SanityManagerAccess {
-	public SanityManager getSanityManager();
-}
